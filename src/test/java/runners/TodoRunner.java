ffb26679c0e44e14f3a663fc27b9f467c9d761e4
Admin <Admin@Gopika>
2025-07-24 19:53:24 +0200

    Latest Commit

--- src/test/java/runners/TodoRunner.java
+++ src/test/java/runners/TodoRunner.java
@@ -1,13 +1,10 @@
 package runners;
 
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
+    features = "src/test/resources/todo_features",
     glue = {"stepDefinitions"},
     plugin = {
         "pretty",
@@ -16,5 +13,6 @@ import io.cucumber.junit.CucumberOptions;
     },
     monochrome = true
 )
-public class TestRunner {
+public class TodoRunner extends AbstractTestNGCucumberTests {
+
 }
